59c8ba15f39e283d1bf7a4530896795d821834d7
Magnus Markling <magnus@markling.com>
2022-10-07 14:47:10 +0200

    parse cmd line arg

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,31 @@
+use std::fs;
+
+use clap::Parser;
+use git2::Repository;
+
+#[derive(Parser, Debug)]
+#[command(author, version)]
+struct Args {
+    #[arg(default_value_t = String::from("alexcrichton/git2-rs"))]
+    github_repo: String,
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let tempdir = tempfile::tempdir().unwrap();
+    let args = Args::parse();
+
+    let tempdir = tempfile::tempdir()?;
     println!("{:?}\n", tempdir);
 
-    let url = "https://github.com/alexcrichton/git2-rs";
-    let _repo = git2::Repository::clone(url, &tempdir)?;
+    let url = format!("https://github.com/{}", args.github_repo);
+    Repository::clone(&url, &tempdir)?;
 
-    let paths = std::fs::read_dir(&tempdir)?;
+    let paths = fs::read_dir(&tempdir)?;
     for path in paths {
-        println!("Name: {}", path.unwrap().path().display());
+        println!("{}", path?.path().display());
     }
 
     Ok(())
 }
+
+// Kör rustfmt (finns API?)
+// Skriv ut om repot är välformatterat eller ej
